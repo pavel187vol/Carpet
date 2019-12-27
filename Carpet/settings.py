27fb63a6a5bee577ca89ab0fa6dacef71510761e
pavel187vol <voloscovich.pavel@gmail.com>
2019-12-27 13:15:46 +0300

    Apps Profiles

--- Carpet/settings.py
+++ Carpet/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles.apps.ProfilesConfig'
+    'phonenumber_field',
+    'profiles.apps.ProfilesConfig',
 ]
 
 MIDDLEWARE = [
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 class AbstractProfile(models.Model):
-    user = models.ForeignKey(User, rel)
+    user = models.ForeignKey(User,
+                             related_name='%(class)s_related',
+                             on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    phone = PhoneNumberField(blank=True, unique=True)
+    email = models.EmailField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    feedback = models.ForeignKey(FeedBack,
+                                 related_name='%(class)s_related',
+                                 on_delete=models.SET_NULL)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return "{}. {}".format(self.first_name,
+        self.last_name)
+
+class Customer(AbstractProfile):
+    order = models.ForeignKey(Order,
+                              related_name='customer',
+                              on_delete=models.SET_NULL)
+
+class Executer(AbstractProfile):
+    description = models.ForeignKey(DescriptionExecuter,
+                                    related_name='executer',
+                                    on_delete=models.SET_NULL)
+    work = models.ForeignKey(Order,
+                             related_name='executer',
+                             on_delete=models.SET_NULL)
+
+class Order(models.Model):
+    description_order = models.ForeignKey(DescriptionOrder,
+                                          related_name='order',
+                                          on_delete=models.SET_NULL)
+    
