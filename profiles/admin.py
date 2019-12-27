99c496f60b0d13a1545ac6b58d369879bc9abfd3
pavel187vol <voloscovich.pavel@gmail.com>
2019-12-27 15:21:07 +0300

    Done Models

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Customer, Executer,\
+                    Order, TypeWork, DescriptionOrder
 
-# Register your models here.
+@admin.register(Customer)
+class CumstomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone',
+                    'email', 'order']
+    list_filter = ['order', 'first_name']
+    search_fields = ['first_name', 'last_name', 'order'
+                     'phone', 'email']
+
+@admin.register(Executer)
+class ExecuterAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone',
+                    'email']
+    list_filter = ['first_name']
+    search_fields = ['first_name', 'last_name', 'description'
+                     'phone', 'email']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'executor', 'condition']
+    list_filter = ['condition']
+    search_fields = ['customer', 'executor']
+
+@admin.register(TypeWork)
+class TypeWorkAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+
+@admin.register(DescriptionOrder)
+class DescriptionOrderAdmin(admin.ModelAdmin):
+    list_display = ['order', 'type_work', 'price']
+    list_filter = ['price']
+    search_fields = ['type_work', 'price']
--- profiles/models.py
+++ profiles/models.py
@@ -11,32 +11,73 @@ class AbstractProfile(models.Model):
     phone = PhoneNumberField(blank=True, unique=True)
     email = models.EmailField(unique=True)
     created = models.DateTimeField(auto_now_add=True)
-    feedback = models.ForeignKey(FeedBack,
-                                 related_name='%(class)s_related',
-                                 on_delete=models.SET_NULL)
+    # feedback = models.ForeignKey('FeedBack',
+    #                              related_name='%(class)s_related',
+    #                              on_delete=models.SET_NULL,
+    #                              blank=True)
 
     class Meta:
         abstract = True
 
     def __str__(self):
         return "{}. {}".format(self.first_name,
-        self.last_name)
+                               self.last_name)
 
 class Customer(AbstractProfile):
-    order = models.ForeignKey(Order,
-                              related_name='customer',
-                              on_delete=models.SET_NULL)
+    order = models.ForeignKey('Order',
+                              related_name='customers',
+                              on_delete=models.SET_NULL,
+                              blank=True,
+                              null=True)
 
 class Executer(AbstractProfile):
-    description = models.ForeignKey(DescriptionExecuter,
+    description = models.ForeignKey('DescriptionExecuter',
                                     related_name='executer',
-                                    on_delete=models.SET_NULL)
-    work = models.ForeignKey(Order,
-                             related_name='executer',
-                             on_delete=models.SET_NULL)
+                                    on_delete=models.SET_NULL,
+                                    blank=True,
+                                    null=True)
+    work = models.ForeignKey('Order',
+                             related_name='executers',
+                             on_delete=models.SET_NULL,
+                             blank=True,
+                             null=True)
 
 class Order(models.Model):
-    description_order = models.ForeignKey(DescriptionOrder,
-                                          related_name='order',
-                                          on_delete=models.SET_NULL)
-    
+    description_order = models.ForeignKey('DescriptionOrder',
+                                          related_name='orders',
+                                          on_delete=models.CASCADE)
+    customer = models.ForeignKey('Customer',
+                                 related_name='customers',
+                                 on_delete=models.CASCADE)
+    executor = models.ForeignKey('Executer',
+                                 related_name='executors',
+                                 on_delete=models.SET_NULL,
+                                 blank=True,
+                                 null=True)
+    condition = models.BooleanField(default=False)
+
+class DescriptionOrder(models.Model):
+    text = models.TextField()
+    image = models.ImageField(upload_to='orders/%Y/%m/%d',
+                              blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    type_work = models.ForeignKey('TypeWork',
+                                  related_name='descriptionss',
+                                  on_delete=models.SET_NULL,
+                                  blank=True,
+                                  null=True)
+    order = models.ForeignKey('Order',
+                              related_name='orders',
+                              on_delete=models.CASCADE)
+
+class TypeWork(models.Model):
+    title = models.CharField(max_length=200)
+
+class FeedBack(models.Model):
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class DescriptionExecuter(models.Model):
+    pass
